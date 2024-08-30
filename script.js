f1117b0473d8bb0ce197157d13d19057ccefac58
King William <seanapowi@gmail.com>
2024-08-30 17:08:27 +0300

    comment the displayMessage function

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ let secreteNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
 
+//function to refactory repeated (message) code
 const dispplayMssage = function (message) {
   document.querySelector('.message').textContent = message;
 };
